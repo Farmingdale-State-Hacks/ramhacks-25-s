a746ce8c224cb374eb08169c649b055591748b15
Mike Odnis <mike@mikeodnis.dev>
2026-07-27 19:13:36 -0400

    fix: safely guard undefined event in toWebRequest in Nitro patch

--- scripts/patch-nitro.js
+++ scripts/patch-nitro.js
@@ -9,7 +9,7 @@ if (fs.existsSync(p)) {
   );
   code = code.replace(
     /import\s*\{[^}]*eventHandler[^}]*\}\s*from\s*['"]@tanstack\/start-server-core['"];?/g,
-    "import { eventHandler as eventHandler$1, getResponseStatus } from 'h3'; import { createStartHandler } from '@tanstack/start-server-core'; const getEvent = (event) => event; const toWebRequest = (event) => event?.request || event;"
+    "import { eventHandler as eventHandler$1, getResponseStatus } from 'h3'; import { createStartHandler } from '@tanstack/start-server-core'; const getEvent = (event) => event || {}; const toWebRequest = (event) => event ? (event.request || event) : new Request('http://localhost:3000');"
   );
   code = code.replace(
     /import\s*\{[^}]*startSerializer[^}]*\}\s*from\s*['"]@tanstack\/start-client-core['"];?/g,

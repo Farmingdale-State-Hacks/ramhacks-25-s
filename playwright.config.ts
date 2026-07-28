1a6e72f294037308632368c56e7458d106ec0505
Mike Odnis <mike@mikeodnis.dev>
2026-07-28 19:48:01 -0400

    fix: enable reuseExistingServer in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -133,7 +133,7 @@ export default defineConfig({
     webServer: {
       command: 'PORT=3000 node .output/server/index.mjs',
       url: 'http://127.0.0.1:3000',
-      reuseExistingServer: !process.env.CI,
+      reuseExistingServer: true,
       timeout: Number(process.env.PLAYWRIGHT_WEBSERVER_TIMEOUT) || 120000,
     }
   } : {})
